ecd13894841c2f8b6bef7f58ce5d0c0464a604cb
Kenneth Cole <70181196+coleken@users.noreply.github.com>
2020-12-12 04:15:04 -0600

    feat: Update the browse fragment

--- app/src/main/java/com/example/newsq/ui/browse/BrowseFragment.java
+++ app/src/main/java/com/example/newsq/ui/browse/BrowseFragment.java
@@ -1,13 +1,18 @@
 package com.example.newsq.ui.browse;
 
+import static android.R.layout.simple_spinner_dropdown_item;
+import static android.R.layout.simple_spinner_item;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import com.example.newsq.R;
 import com.example.newsq.databinding.FragmentBrowseBinding;
 
 /**
@@ -30,7 +35,33 @@ public class BrowseFragment extends Fragment implements AdapterView.OnItemSelect
   public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
     binding = FragmentBrowseBinding.inflate(inflater, container, false);
+    getBrowseSpinner();
     return binding.getRoot();
+  }
+
+  /**
+   * Returns a spinner configured to display news categories.
+   *
+   * @return A spinner configured to use the sections array adapter.
+   */
+  private Spinner getBrowseSpinner() {
+    Spinner spinner = binding.spinnerSections;
+    spinner.setAdapter(createSelectionAdapter());
+    spinner.setOnItemSelectedListener(this);
+    return spinner;
+  }
+
+  /**
+   * Returns a string adapter for the browse fragment spinner.
+   *
+   * @return A string array adapter populated with the news sections string array.
+   */
+  private ArrayAdapter<String> createSelectionAdapter() {
+    String[] selections = getResources().getStringArray(R.array.spinner_array);
+    ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), simple_spinner_item,
+        selections);
+    adapter.setDropDownViewResource(simple_spinner_dropdown_item);
+    return adapter;
   }
 
   @Override
@@ -47,30 +78,5 @@ public class BrowseFragment extends Fragment implements AdapterView.OnItemSelect
   @Override
   public void onNothingSelected(AdapterView<?> parent) {
     // TODO: onNothingSelected
-  }
-
-  /**
-   * TODO
-   */
-  private void displayNewsStories() {
-    // TODO: displayNewsStories
-  }
-
-  /**
-   * TODO
-   */
-  private Spinner getSpinner() {
-    // TODO: buildSpinner
-    return null;
-  }
-
-  /**
-   * TODO
-   *
-   * @return A string array with news sections.
-   */
-  private String[] getSections() {
-    // TODO: getSelections
-    return null;
   }
 }
